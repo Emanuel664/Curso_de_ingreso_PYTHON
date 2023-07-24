ebe8d46cf83c1be6b622321473b63b716cad6ff7
Emanuel <manuvazquez664@gmail.com>
2023-07-24 12:33:42 -0300

    Tp 6

--- ejercicios/07_tps/06_WHILE_Elecciones_Paso/app.py
+++ ejercicios/07_tps/06_WHILE_Elecciones_Paso/app.py
@@ -30,19 +30,71 @@ class App(customtkinter.CTk):
 
     def btn_validar_on_click(self):
         
+        ciclo = True
+        maximo_de_votos = -1
+        contador = 0
+        bandera = True
+        nombre_maximo_votos = ""
+        nombre_minimo_votos = ""
+        minimo_de_votos = None
+        edad_minima = None
+        edad_maxima = None
+        acumulador_de_edad = 0
+        acumulador_de_votos = 0
+
+       
         while True:
+
             nombre = prompt(title = "Nombre", prompt = "Ingrese el nombre del candidato")
-            while (nombre == None or nombre == "") or not nombre.isalpha():
+            while  nombre == None  or not nombre.isalpha():
                 nombre = prompt("Nombre","Ingrese el nombre del candidato")
 
             edad = prompt("Edad", "Ingrese su edad")
             while edad == None or not edad.isdigit() or int(edad) < 25:
                 edad = prompt("Edad", "Ingrese una edad valida")
 
+            edad = int(edad)
+
             cantidad_de_votos = prompt("Votos", "Cantidad de votos")
             while cantidad_de_votos == None or not cantidad_de_votos.isdigit() or int(cantidad_de_votos) <= 0:
                 cantidad_de_votos = prompt("Error", "Ingrese una cantidad valida")
-                
+            
+            cantidad_de_votos = int(cantidad_de_votos)
+
+            if bandera == True:
+                maximo_de_votos = cantidad_de_votos
+                minimo_de_votos = cantidad_de_votos
+                nombre_maximo_votos = nombre
+                nombre_minimo_votos = nombre
+                edad_minima = edad
+                edad_maxima = edad
+                bandera = False
+
+            else:
+                if cantidad_de_votos > maximo_de_votos:
+                    maximo_de_votos = cantidad_de_votos
+                    nombre_maximo_votos = nombre
+                    edad_maxima = edad
+                elif cantidad_de_votos < minimo_de_votos:
+                    minimo_de_votos = cantidad_de_votos
+                    nombre_minimo_votos = nombre
+                    edad_minima = edad
+
+            acumulador_de_edad += edad
+            contador += 1
+            acumulador_de_votos += cantidad_de_votos
+
+
+            pregunta = question("Pregunta", "¿Desea continuar?")
+
+            if not pregunta:
+             break  
+        
+        promedio = acumulador_de_edad / contador
+        print(f"el candidato con mayor numeros de votos es {nombre_maximo_votos} con {maximo_de_votos} votos")
+        print(f"el candidato con menor numeros de votos es {nombre_minimo_votos} de {edad_minima} años con {minimo_de_votos} votos")
+        print(f"Promedio de edad {promedio}")
+        print(f"Total de votos {acumulador_de_votos}")     
 
 
 
